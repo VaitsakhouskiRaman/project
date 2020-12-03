160c7fcc5f671a39b97d9f8778801aa8bb5a7789
Raman Vaitsakhouski <you@example.com>
2020-12-03 13:34:14 +0100

    rebuild dir

---
+++ JS/firstJS/js/script.js
@@ -0,0 +1,18 @@
+let money = prompt("Ваш бюджет на месяц?", '');
+let time = prompt("Введите дату в формате YYYY-MM-DD", '');
+let firstAnswer = prompt("Введите обязательную статью расходов в этом месяце", '');
+let secondAnswer = prompt("Во сколько обойдется?", '');
+let moneyPerDay = (money / 30);
+let expenses = {firstAnswer, secondAnswer};
+let optionalExpenses = {};
+let income = [];
+let savings = false;
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses,
+    optionalExpenses, 
+    income,
+    savings
+};
+alert("your budget on 1 day is - " + moneyPerDay);
